03376aa98919359b5cf15db6ce181e67002f2dc5
it11-2016 <it11-2016@AR4-ASISTENT.ftn.ns>
2022-03-14 08:15:40 +0100

    Druge vezbe - prvi deo

--- Grupa_1_2022/src/introduction/HelloWorld.java
+++ Grupa_1_2022/src/introduction/HelloWorld.java
@@ -5,6 +5,56 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
+		double firstNumber=5;
+		double secondNumber=2;
+		
+		int  rez=(int)(firstNumber/secondNumber);
+		
+		System.out.println("Rezultat je "+rez);
+		
+		System.out.println(5+rez);
+		
+		boolean alwaysTrue=true;
+		boolean alwaysFalse=false;
+		
+		System.out.println(alwaysTrue && alwaysFalse);
+		System.out.println(alwaysTrue || alwaysFalse);
+		System.out.println(!alwaysTrue);
+		
+		//1. Zadatak
+		System.out.println("Prvi zadatak");
+		int a = 24;
+		int b = 12;
+		String operation = "-";
+		if (operation == "+")
+			System.out.println(a + b);
+		else if (operation == "-")
+			System.out.println(a - b);
+		else if (operation == "/")
+			System.out.println(a / b);
+		else
+			System.out.println(a * b);
+		
+		//2. Zadatak
+		int i, faktorijel1=1;
+		int broj=5;
+		for(i=1;i<=broj;i++)
+		{
+			faktorijel1=faktorijel1*i;
+		}System.out.println(faktorijel1);
+		
+		//3. Zadatak
+		
+		for(i=1;i<10;i++) {
+			if(i%2==1) {
+				continue;
+				//break;
+			}
+			System.out.println("\n" + i);
+		}
+		
+		
+		
 
 	}
 
